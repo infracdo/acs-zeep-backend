5eec3ba5b977f39d8a84716bede941a55e5e5edf
infracdo <infracdo@gmail.com>
2025-07-10 10:00:18 +0800

    Update AllowedNasMacAddress.java

--- src/main/java/com/acs_tr069/test_tr069/radius/entity/AllowedNasMacAddress.java
+++ src/main/java/com/acs_tr069/test_tr069/radius/entity/AllowedNasMacAddress.java
@@ -14,7 +14,7 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "allowed_nas_mac_address") 
+@Table(name = "allowed_nas_mac_address", schema = "public")
 public class AllowedNasMacAddress {
     
     @Id
