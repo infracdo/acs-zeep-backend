afde7f1cd889295f722ac1eb77623b4680ae9d13
infracdo <infracdo@gmail.com>
2025-08-13 15:39:29 +0800

    commented out zeep wifidog

--- src/main/java/com/acs_tr069/test_tr069/zeep/controller/MonitoringController.java
+++ src/main/java/com/acs_tr069/test_tr069/zeep/controller/MonitoringController.java
@@ -1,140 +1,140 @@
-package com.acs_tr069.test_tr069.zeep.controller;
+// package com.acs_tr069.test_tr069.zeep.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.LinkedHashMap;
+// import java.util.List;
+// import java.util.Map;
+// import java.util.stream.Collectors;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.acs_tr069.test_tr069.zeep.service.MonitoringService;
+// import com.acs_tr069.test_tr069.zeep.service.MonitoringService;
 
-@RestController
-@RequestMapping("/api/wifidog/")
-@CrossOrigin("*")
-public class MonitoringController {
+// @RestController
+// @RequestMapping("/api/wifidog/")
+// @CrossOrigin("*")
+// public class MonitoringController {
 
-    private final MonitoringService monitoringService;
+//     private final MonitoringService monitoringService;
 
-    public MonitoringController(MonitoringService monitoringService) {
-        this.monitoringService = monitoringService;
-    }
+//     public MonitoringController(MonitoringService monitoringService) {
+//         this.monitoringService = monitoringService;
+//     }
 
-    // Get current number of connected users
-    @GetMapping("/count-current-connected-users")
-    public ResponseEntity<Map<String, Object>> getCountCurrentConnectedUsers() {
-        long countConnectedUsers = monitoringService.getCountCurrentConnectedUsers();
+//     // Get current number of connected users
+//     @GetMapping("/count-current-connected-users")
+//     public ResponseEntity<Map<String, Object>> getCountCurrentConnectedUsers() {
+//         long countConnectedUsers = monitoringService.getCountCurrentConnectedUsers();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("connectedUsers", countConnectedUsers);
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("connectedUsers", countConnectedUsers);
 
-        return ResponseEntity.ok(response);
-    }
+//         return ResponseEntity.ok(response);
+//     }
 
-    // Get current number of connected access points (APs)
-    @GetMapping("/count-current-connected-aps")
-    public ResponseEntity<Map<String, Object>> getCountCurrentConnectedAPs() {
-        long countConnectedAPs = monitoringService.getCountCurrentConnectedAPs();
+//     // Get current number of connected access points (APs)
+//     @GetMapping("/count-current-connected-aps")
+//     public ResponseEntity<Map<String, Object>> getCountCurrentConnectedAPs() {
+//         long countConnectedAPs = monitoringService.getCountCurrentConnectedAPs();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("connectedAPs", countConnectedAPs);
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("connectedAPs", countConnectedAPs);
 
-        return ResponseEntity.ok(response);
-    }
+//         return ResponseEntity.ok(response);
+//     }
 
-    // Get total user connections for today
-    @GetMapping("/total-user-connections-today")
-    public ResponseEntity<Map<String, Object>> getTotalUserConnectionsToday() {
-        long totalConnectedUsers = monitoringService.getTotalUserConnectionsToday();
+//     // Get total user connections for today
+//     @GetMapping("/total-user-connections-today")
+//     public ResponseEntity<Map<String, Object>> getTotalUserConnectionsToday() {
+//         long totalConnectedUsers = monitoringService.getTotalUserConnectionsToday();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("totalUserConnectionsToday", totalConnectedUsers);
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("totalUserConnectionsToday", totalConnectedUsers);
 
-        return ResponseEntity.ok(response);
-    }
+//         return ResponseEntity.ok(response);
+//     }
 
-    // Get total bandwidth consumption for today
-    @GetMapping("/total-bandwidth-consumption-today")
-    public ResponseEntity<Map<String, Object>> getTotalBandwidthConsumptionToday() {
-        double totalBandwidthConsumptionToday = monitoringService.getTotalBandwidthConsumptionToday();
+//     // Get total bandwidth consumption for today
+//     @GetMapping("/total-bandwidth-consumption-today")
+//     public ResponseEntity<Map<String, Object>> getTotalBandwidthConsumptionToday() {
+//         double totalBandwidthConsumptionToday = monitoringService.getTotalBandwidthConsumptionToday();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("totalBandwidthConsumptionToday", totalBandwidthConsumptionToday);
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("totalBandwidthConsumptionToday", totalBandwidthConsumptionToday);
 
-        return ResponseEntity.ok(response);
-    }
+//         return ResponseEntity.ok(response);
+//     }
 
-    // Get average connection time
-    @GetMapping("/avg-connection-time")
-    public ResponseEntity<Map<String, Object>> getAvgConnectionTime() {
-        double averageConnectionTime = monitoringService.getAvgConnectionTime();
+//     // Get average connection time
+//     @GetMapping("/avg-connection-time")
+//     public ResponseEntity<Map<String, Object>> getAvgConnectionTime() {
+//         double averageConnectionTime = monitoringService.getAvgConnectionTime();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("averageConnectionTime", averageConnectionTime);
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("averageConnectionTime", averageConnectionTime);
 
-        return ResponseEntity.ok(response);
+//         return ResponseEntity.ok(response);
 
-    }
+//     }
 
-    // Get average bandwidth per connection
-    @GetMapping("/average-bandwidth-per-connection")
-    public ResponseEntity<Map<String, Object>> getAvgBandwidthPerConnection() {
-        double avgBandwidthPerConnection = monitoringService.getAvgBandwidthPerConnection();
+//     // Get average bandwidth per connection
+//     @GetMapping("/average-bandwidth-per-connection")
+//     public ResponseEntity<Map<String, Object>> getAvgBandwidthPerConnection() {
+//         double avgBandwidthPerConnection = monitoringService.getAvgBandwidthPerConnection();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("averageBandwidthPerConnection", avgBandwidthPerConnection);
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("averageBandwidthPerConnection", avgBandwidthPerConnection);
 
-        return ResponseEntity.ok(response);
-    }
+//         return ResponseEntity.ok(response);
+//     }
 
-    // Get number of currently connected users per access point (AP)
-    @GetMapping("/count-current-connected-users-per-ap")
-    public ResponseEntity<List<Map<String, Object>>> getCountCurrentConnectedUsersPerAp() {
-        Map<String, Long> counts = monitoringService.getCountCurrentConnectedUsersPerAP();
+//     // Get number of currently connected users per access point (AP)
+//     @GetMapping("/count-current-connected-users-per-ap")
+//     public ResponseEntity<List<Map<String, Object>>> getCountCurrentConnectedUsersPerAp() {
+//         Map<String, Long> counts = monitoringService.getCountCurrentConnectedUsersPerAP();
         
-        List<Map<String, Object>> response = counts.entrySet().stream()
-            .map(entry -> {
-                Map<String, Object> map = new LinkedHashMap<>();
-                map.put("apMacAddress", entry.getKey());
-                map.put("userCount", entry.getValue());
-                return map;
-            })
-            .collect(Collectors.toList());
+//         List<Map<String, Object>> response = counts.entrySet().stream()
+//             .map(entry -> {
+//                 Map<String, Object> map = new LinkedHashMap<>();
+//                 map.put("apMacAddress", entry.getKey());
+//                 map.put("userCount", entry.getValue());
+//                 return map;
+//             })
+//             .collect(Collectors.toList());
 
-        return ResponseEntity.ok(response);
-    }
+//         return ResponseEntity.ok(response);
+//     }
 
-    // Get list of currently connected users per access point (AP)
-    @GetMapping("/current-connected-users-per-ap")
-    public ResponseEntity<List<Map<String, Object>>> getCurrentConnectedUsersPerAP() {
-        Map<String, List<Map<String, Object>>> currentConnectedUsersPerAP = monitoringService.getCurrentConnectedUsersPerAP();
-        List<Map<String, Object>> response = new ArrayList<>();
+//     // Get list of currently connected users per access point (AP)
+//     @GetMapping("/current-connected-users-per-ap")
+//     public ResponseEntity<List<Map<String, Object>>> getCurrentConnectedUsersPerAP() {
+//         Map<String, List<Map<String, Object>>> currentConnectedUsersPerAP = monitoringService.getCurrentConnectedUsersPerAP();
+//         List<Map<String, Object>> response = new ArrayList<>();
 
-        currentConnectedUsersPerAP.forEach((apMac, users) -> {
-            Map<String, Object> entry = new HashMap<>();
-            entry.put("apMacAddress", apMac);
-            entry.put("connectedUsers", users);
-            response.add(entry);
-        });
+//         currentConnectedUsersPerAP.forEach((apMac, users) -> {
+//             Map<String, Object> entry = new HashMap<>();
+//             entry.put("apMacAddress", apMac);
+//             entry.put("connectedUsers", users);
+//             response.add(entry);
+//         });
 
-        return ResponseEntity.ok(response);
-    }
+//         return ResponseEntity.ok(response);
+//     }
 
-    // Get list of currently connected access points (AP)
-    @GetMapping("/current-connected-aps")
-    public ResponseEntity<Map<String, Object>> getCurrentConnectedAPs() {
-        List<String> connectedAPs = monitoringService.getCurrentConnectedAPs();
+//     // Get list of currently connected access points (AP)
+//     @GetMapping("/current-connected-aps")
+//     public ResponseEntity<Map<String, Object>> getCurrentConnectedAPs() {
+//         List<String> connectedAPs = monitoringService.getCurrentConnectedAPs();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("status", "success");
-        response.put("message", "Currently connected access points retrieved successfully.");
-        response.put("data", connectedAPs);
-        return ResponseEntity.ok(response);
-    }
-}
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("status", "success");
+//         response.put("message", "Currently connected access points retrieved successfully.");
+//         response.put("data", connectedAPs);
+//         return ResponseEntity.ok(response);
+//     }
+// }
--- src/main/java/com/acs_tr069/test_tr069/zeep/entity/AccSessions.java
+++ src/main/java/com/acs_tr069/test_tr069/zeep/entity/AccSessions.java
@@ -1,49 +1,49 @@
-package com.acs_tr069.test_tr069.zeep.entity;
+// package com.acs_tr069.test_tr069.zeep.entity;
 
-import java.time.Instant;
+// import java.time.Instant;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
 
-import lombok.Data;
+// import lombok.Data;
 
-@Entity
-@Data
-@Table(name = "acc_sessions")
-public class AccSessions {
+// @Entity
+// @Data
+// @Table(name = "acc_sessions")
+// public class AccSessions {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @Column(name = "account_Number")
-    private String accountNumber;
+//     @Column(name = "account_Number")
+//     private String accountNumber;
 
-    @Column(name = "package")
-    private String packageType;
+//     @Column(name = "package")
+//     private String packageType;
 
-    @Column(name = "limit_count")
-    private Integer limitCount;
+//     @Column(name = "limit_count")
+//     private Integer limitCount;
 
-    @Column(name = "limit_type")
-    private String limitType;
+//     @Column(name = "limit_type")
+//     private String limitType;
 
-    @Column(name = "counter")
-    private Integer counter;
+//     @Column(name = "counter")
+//     private Integer counter;
 
-    @Column(name = "incoming_packets", columnDefinition = "float8")
-    private Double incomingPackets;
+//     @Column(name = "incoming_packets", columnDefinition = "float8")
+//     private Double incomingPackets;
 
-    @Column(name = "outgoing_packets", columnDefinition = "float8")
-    private Double outgoingPackets;
+//     @Column(name = "outgoing_packets", columnDefinition = "float8")
+//     private Double outgoingPackets;
 
-    @Column(name = "created_on", columnDefinition = "timestamptz")
-    private Instant createdOn;
+//     @Column(name = "created_on", columnDefinition = "timestamptz")
+//     private Instant createdOn;
 
-    @Column(name = "last_modified")
-    private String lastModified;
-}
+//     @Column(name = "last_modified")
+//     private String lastModified;
+// }
